956a76b34a1430bc1dea95b37f2d1a12294d10e3
Patrick Teulings <patrickteulings@gmail.com>
2020-04-07 09:32:34 +0200

    Changed homepage text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,11 @@
     <div class="links__home">
     <h1>Hi there!</h1>
     <p>Omdat we allemaal leuke tips hebben om overdag met onze kinderen bezig zijn.</p>
-    <p>Omdat we allemaal leuke tips hebben als ze 's-avonds in bed liggen'.</p>
     <p>Omdat we allemaal zoveel mogelijk binnen moeten blijven...🙁</p>
     <p>En ik eindeloos door WhatsApp lijsten scroll om die ene link op te zoeken...</p>
     <p>&nbsp;</p>
-    <p>Dit overzicht met leuke links voor ons, onze wat grotere kleintjes en onze wat kleinere kleintjes.</p>
-    <p>Links in het menu zie je de secties. Daar kun je ook items toevoegen (graag!!!!)</p>
+    <p>Daarom heb ik dit platformpje gebouwd met links voor ons, onze wat grotere kleintjes en onze wat kleinere kleintjes.</p>
+    <p>Ik heb daarbij wel jullie hulp nodig, dus kom maar op met die links! In het menu (links) kun je ze toevoegen!</p>
     <p>Heb je vragen, app me even persoonlijk!</p>
     <p>Grt! Patrick (0614479205)</p>
     </div>
@@ -51,3 +50,9 @@ export default Vue.extend({
   }
 });
 </script>
+
+<style scoped>
+  p {
+    font-weight: 400;
+  }
+</style>
